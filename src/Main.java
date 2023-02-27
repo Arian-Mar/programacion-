ad91798538fea78818411a63685e48164af3cb3a
Arian-Mar <lopez.ariana.3fv@gmail.com>
2023-02-26 22:13:40 -0600

    tarea1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Escribe tu nombre, profesion y pais");
+        Scanner teclado = new Scanner(System.in);
+        String entrada = teclado.nextLine();
+        String[] separado = entrada.toUpperCase().split(" ");
+
+        System.out.println("nombre: "+separado[0]);
+        System.out.println("profesion: "+separado[1]);
+        System.out.println("pais: "+separado[2]);
+
     }
 }
